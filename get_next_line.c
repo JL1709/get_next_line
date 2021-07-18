dc7af6a9b3daefa2bff03983f5ea09634587d3a3
Julian Ludt <julian@Julians-Air.fritz.box>
2021-07-18 15:05:05 +0200

    really works (no norm)

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,11 @@
 /*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 15:42:32 by jludt             #+#    #+#             */
-/*   Updated: 2021/07/17 12:51:14 by julian           ###   ########.fr       */
+/*   Updated: 2021/07/18 15:01:56 by julian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// static char *output2(char )
-// [
-	
-// ]
 
 static char	*prepare_output(char *text)
 {
@@ -26,7 +21,7 @@ static char	*prepare_output(char *text)
 	int			k;
 	int			l;
 	char		nl[2];
-		
+
 	if (temp)
 		text = ft_strjoin2(temp, text);
 	if (ft_strlen(text) == 0)
@@ -55,9 +50,11 @@ static char	*prepare_output(char *text)
 		k = i + 1;
 	j = 0;
 	l = k;
-	while(text[k++] != '\0')
+	while (text[k++] != '\0')
 		j++;
 	temp = (char *)malloc(sizeof(char) * j + 1);
+	if (temp == NULL)
+		return (NULL);
 	j = 0;
 	while (text[l] != '\0')
 		temp[j++] = text[l++];
@@ -66,14 +63,19 @@ static char	*prepare_output(char *text)
 	if (text[i] == '\n')
 		return (ft_strjoin(line, nl));
 	else
+	{
+		free(temp);
+		temp = NULL;
 		return (line);
+	}
 }
+
 
 char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE + 1];
-	char	*text;
-	int		buff_size;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*text;
+	int			buff_size;
 
 	text = (char *)malloc(sizeof(char) * 1);
 	if (text == NULL || BUFFER_SIZE <= 0)
@@ -97,61 +99,13 @@ char	*get_next_line(int fd)
 	return (prepare_output(text));
 }
 
-// int	main(void)
-// {
-// 	int	i = open("test.txt", O_RDONLY);
-// 	for (int j = 0; j < 5; j++)
-// 	{
-// 		printf("%s", get_next_line(i));
-// 	}
-// 	close(i);
-// 	return (0);
-// }
-
-
-
-
-// static char	*output(char *text)
-// {
-// 	char		*line;
-// 	static char	temp[1000];
-// 	int			i;
-// 	int			j;
-// 	int			k;
-// 	char		nl[2];
-		
-// 	text = ft_strjoin2(temp, text);
-// 	if (ft_strlen(text) == 0)
-// 	{
-// 		free(text);
-// 		return (NULL);
-// 	}
-// 	nl[0] = '\n';
-// 	nl[1] = '\0';
-// 	i = 0;
-// 	while (text[i] != '\n' && text[i] != '\0')
-// 		i++;
-// 	line = (char *)malloc(sizeof(char) * i + 1);
-// 	if (line == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (text[i] != '\n' && text[i] != '\0')
-// 	{
-// 		line[i] = text[i];
-// 		i++;
-// 	}
-// 	line[i] = '\0';
-// 	if (text[i] == '\0')
-// 		k = i;
-// 	else
-// 		k = i + 1;
-// 	j = 0;
-// 	while(text[k] != '\0')
-// 		temp[j++] = text[k++];
-// 	temp[j] = '\0';
-// 	free(text);
-// 	if (text[i] == '\n')
-// 		return (ft_strjoin(line, nl));
-// 	else
-// 		return (line);
-// }
+int	main(void)
+{
+	int	i = open("test.txt", O_RDONLY);
+	for (int j = 0; j < 5; j++)
+	{
+		printf("%s", get_next_line(i));
+	}
+	close(i);
+	return (0);
+}
